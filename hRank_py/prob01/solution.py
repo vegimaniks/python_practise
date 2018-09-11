fb3104c24430861c579b707601d2806dc029a490
vegimaniks <vegimani87@gmail.com>
2018-09-12 00:42:48 +0530

    Datatypes
    Learn the input string
    convert the string to int
    convet the string to float

---
+++ hRank_py/prob01/solution.py
@@ -0,0 +1,26 @@
+n = input()
+score = str(input())
+scores = score.split(" ")
+print(scores)
+print(max(scores))
+    for i in range(0,len(scores))
+	    if scores == max(scores)
+		     print(max(scores))
+			 
+			 
+		
+	
+ 
+ 
+
+
+
+
+
+
+#li = list(set([1,2,5,4,4, 7,7]))
+#li.sort()
+#print(li[-2])
+
+
+
